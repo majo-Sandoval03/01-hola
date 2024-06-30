f8e31765eaffc9ac009434695f428690b9badda2
Majo <majito_1899@hotmail.com>
2024-06-30 10:58:37 -0500

    Funciona Hola Mundo

---
+++ 01_HolaMundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("Hola Mundo");
+   //Esto es un comentario pequeño
+   //Indicar que el programa termino correctamente
+   return 0;
+}
+
+/*Esto es un comentario largo*/
